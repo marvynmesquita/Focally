c53b33ff366994a406ae04b1e390aff13509d2bc
Marvyn Mesquita <31486769+marvynmesquita@users.noreply.github.com>
2025-11-05 20:42:16 -0300

    Adiciona servidores STUN e TURN para melhorar a conectividade no WebRTC

--- src/useWebRTC.js
+++ src/useWebRTC.js
@@ -25,7 +25,30 @@ export const useWebRTC = (mode) => {
   const rtcConfig = {
     iceServers: [
       { urls: 'stun:stun.l.google.com:19302' },
-      { urls: 'stun:stun1.l.google.com:19302' }
+      { urls: 'stun:stun1.l.google.com:19302' },
+      {
+        urls: "stun:stun.relay.metered.ca:80",
+      },
+      {
+        urls: "turn:standard.relay.metered.ca:80",
+        username: "135917c2e0b8d6a968c34211",
+        credential: "KiW8fcC8uTk/5lCa",
+      },
+      {
+        urls: "turn:standard.relay.metered.ca:80?transport=tcp",
+        username: "135917c2e0b8d6a968c34211",
+        credential: "KiW8fcC8uTk/5lCa",
+      },
+      {
+        urls: "turn:standard.relay.metered.ca:443",
+        username: "135917c2e0b8d6a968c34211",
+        credential: "KiW8fcC8uTk/5lCa",
+      },
+      {
+        urls: "turns:standard.relay.metered.ca:443?transport=tcp",
+        username: "135917c2e0b8d6a968c34211",
+        credential: "KiW8fcC8uTk/5lCa",
+      },
     ]
   };
 
