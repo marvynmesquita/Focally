2beadcfa49516b6a9e269acc56f37c72c4799cf0
Marvyn Mesquita <31486769+marvynmesquita@users.noreply.github.com>
2025-11-05 16:24:42 -0300

    Add allowed hosts to Vite configuration for improved server accessibility

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const port = parseInt(process.env.PORT || '4000', 10)
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['focally.onrender.com', 'localhost', '127.0.0.1'],
     port: port,
     host: '0.0.0.0', // Escuta em todas as interfaces (necessário para Render)
     strictPort: true, // Falha se a porta estiver em uso
